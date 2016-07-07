9316ec68b7a25f5c96ad017bc7882bc58c3cb39c
isztwankebke <isztwan.kebke@gmail.com>
2016-07-07 16:37:18 +0200

    Add files via upload

---
+++ public_html/js/obliczanie-daty-konca.js
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+function oblicz(){
+a=document.DodajKlienta.DataZawarcia.value
+b=document.DodajKlienta.OkresKredytu.value 
+document.DodajKlienta.DataKoniec.value=a; 
+if (document.DodajKlienta.DataKoniec.value=="NaN"){ 
+document.DodajKlienta.DataKoniec.value="";};} 
